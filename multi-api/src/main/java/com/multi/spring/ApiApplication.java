652cc0f552c0ba552ecab4129de6d1f5285e9f8b
sgh15487 <sgh01101@naver.com>
2022-09-03 17:03:02 +0900

    1. multi-api 추가

---
+++ multi-api/src/main/java/com/multi/spring/ApiApplication.java
@@ -0,0 +1,15 @@
+package com.multi.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+/*@EnableAccessCheck*/
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+}
---
+++ multi-api/src/main/java/com/multi/spring/configuration/BasicConfiguration.java
@@ -0,0 +1,34 @@
+package com.multi.spring.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+
+
+/**
+ * 서비스의 기본 설정값 초기화
+ *
+ */
+@Slf4j
+@Configuration
+@ComponentScan(
+        basePackages = {
+                "com.multi.spring",
+//                "com.multi.process.rest"
+        }
+)
+public class BasicConfiguration {
+//    @Bean
+//    public ProjectData projectData() throws Exception {
+//            Yaml yaml = new Yaml();
+//            try (InputStream in = (new ClassPathResource("project-data.yml")).getInputStream()) {
+//                    ProjectData projectData = yaml.loadAs(in, ProjectData.class);
+//                    return projectData;
+//            }
+//    }
+
+}
