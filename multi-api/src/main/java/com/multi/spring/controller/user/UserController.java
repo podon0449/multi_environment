71d6577574cad0305e2633ca1d3f1b4b49fa35e3
sgh15487 <sgh01101@naver.com>
2022-09-03 18:51:31 +0900

    기존 구성 테스트 진행.

--- multi-api/src/main/java/com/multi/spring/controller/user/UserController.java
+++ multi-api/src/main/java/com/multi/spring/controller/user/UserController.java
@@ -1,31 +1,33 @@
-//package com.multi.spring.controller.user;
-//
-//import com.podong.spring.process.service.user.UserBO;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import lombok.extern.slf4j.Slf4j;
-//import org.apache.coyote.Response;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@Api(tags = "유저-API")
-//@RestController
-//@RequestMapping("/v1/api/user")
-//@Slf4j
-//public class UserController {
-//    @Autowired RestApiService<Response> restApiService;
-//    @Autowired UserBO userBO;
-//
-//    @ApiOperation(value = "유저 정보 API")
-//    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json; charset=utf8")
-//    public User getUserInfo(){
-//        //유저 idx 가져와야함
-//        int userIdx = 1;
-//        //ResponseEntity<Response>  a = restApiService.get("/v1/api/user/info", 1);
-//        //throw new PodongCommonException(ERROR, ERROR_NULL, "error is null");
-//        return userBO.userInfo(userIdx);
-//        //return null;
-//    }
-//}
+package com.multi.spring.controller.user;
+
+import com.multi.process.rest.service.RestApiService;
+import com.podong.domain.user.model.User;
+import com.podong.spring.process.service.user.UserBO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "유저-API")
+@RestController
+@RequestMapping("/v1/api/user")
+@Slf4j
+public class UserController {
+    @Autowired RestApiService<Response> restApiService;
+    @Autowired UserBO userBO;
+
+    @ApiOperation(value = "유저 정보 API")
+    @RequestMapping(value = "/info", method = RequestMethod.GET, produces = "application/json; charset=utf8")
+    public User getUserInfo(){
+        //유저 idx 가져와야함
+        int userIdx = 1;
+        //ResponseEntity<Response>  a = restApiService.get("/v1/api/user/info", 1);
+        //throw new PodongCommonException(ERROR, ERROR_NULL, "error is null");
+        return userBO.userInfo(userIdx);
+        //return null;
+    }
+}
--- multi-api/src/main/java/com/multi/spring/process/service/user/UserBO.java
+++ multi-api/src/main/java/com/multi/spring/process/service/user/UserBO.java
@@ -1,16 +1,16 @@
-//package com.podong.spring.process.service.user;
-//
-//
-//import com.podong.domain.user.model.User;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.stereotype.Service;
-//
-//
-//@Slf4j
-//@Service
-//public class UserBO {
-//    public User userInfo(int userIdx) {
-//        User user = new User();
-//        return user;
-//    }
-//}
+package com.podong.spring.process.service.user;
+
+
+import com.podong.domain.user.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+public class UserBO {
+    public User userInfo(int userIdx) {
+        User user = new User();
+        return user;
+    }
+}
