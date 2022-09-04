a5d3bfc6c75c819e19f8db7337e66e30e46387e4
sgh15487 <sgh01101@naver.com>
2022-09-04 15:49:37 +0900

    process-rest 셋팅.

---
+++ mulit-process-rest/src/main/java/com/multi/process/rest/service/RestApiService.java
@@ -0,0 +1,47 @@
+package com.multi.process.rest.service;
+
+import com.multi.process.rest.annotation.RestServer;
+import com.multi.properties.ProjectData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class RestApiService<T> {
+    private RestTemplate restTemplate;
+    @Autowired private ProjectData projectData;
+    @Autowired
+    public RestApiService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public ResponseEntity<T> get(String url) {
+        return callApiEndpoint(url, HttpMethod.GET,null, (Class<T>)Object.class);
+    }
+    public ResponseEntity<T> get(String url, Object params) {
+        //Object params 벗겨내서 url 셋팅진행.
+        return callApiEndpoint(url, HttpMethod.GET,null, (Class<T>)Object.class);
+    }
+    public ResponseEntity<T> get(String url, Class<T> clazz) {
+        return callApiEndpoint(url, HttpMethod.GET, null, clazz);
+    }
+    public ResponseEntity<T> post(String url, HttpHeaders httpHeaders, Object body) {
+        return callApiEndpoint(url, HttpMethod.POST, body,(Class<T>)Object.class);
+    }
+    public ResponseEntity<T> post(String url , Object body, Class<T> clazz) {
+        return callApiEndpoint(url, HttpMethod.POST, body, clazz);
+    }
+    private ResponseEntity<T> callApiEndpoint(String url, HttpMethod httpMethod, Object body, Class<T> clazz) {
+//        String pullUrl =  projectData.getServers().getUrl().get(RestServer.GOOGLE_AUTH.getKey()) + url;
+        String pullUrl =  projectData.getServers().getUrl().get(RestServer.GOOGLE_AUTH.getKey()) + url;
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Cache-Control", "no-store");
+        headers.set("Pragma", "no-cache");
+        headers.set("Content-Type", "application/json;charset=UTF-8");
+        return restTemplate.exchange(pullUrl, httpMethod, new HttpEntity<>( body, headers), clazz);
+    }
+}
