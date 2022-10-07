024332bd5a4450897f78adc5197c6627409e74bd
포동히 <podong0449@Gmail.com>
2022-10-07 18:28:52 +0900

    profile 분기처리 진행중

---
+++ multi-admin/src/main/java/com/multi/spring/AppRunner.java
@@ -0,0 +1,26 @@
+package com.multi.spring;
+
+import com.multi.process.rest.annotation.EnableStub;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppRunner implements ApplicationRunner {
+
+    @Value("${a.name}")
+    private String name;
+    @Value("${b.age}")
+    private int age;
+    @Value("${c.fullName}")
+    private String fullName;
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println("=============");        System.out.println(name);        System.out.println(age);        System.out.println(fullName);        System.out.println("=============");
+    }
+}
